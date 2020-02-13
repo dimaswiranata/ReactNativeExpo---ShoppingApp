065ba780075bb34c5ec6138a8878cd47d3df315a
dimaswiranata <36686862+dimaswiranata@users.noreply.github.com>
2020-02-14 03:33:38 +0700

    implementation token firebase

--- the-shop-app/store/actions/orders.js
+++ the-shop-app/store/actions/orders.js
@@ -38,9 +38,10 @@ export const fetchOrders = () => {
 };
 
 export const addOrder = (cartItems, totalAmount) => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const date = new Date();
-    const response = await fetch('https://rn-complete-guide-b15d9.firebaseio.com/orders/u1.json', {
+    const response = await fetch(`https://rn-complete-guide-b15d9.firebaseio.com/orders/u1.json?auth=${token}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
--- the-shop-app/store/actions/products.js
+++ the-shop-app/store/actions/products.js
@@ -43,9 +43,10 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const response = await fetch(
-      `https://rn-complete-guide-b15d9.firebaseio.com/products/${productId}.json`,
+      `https://rn-complete-guide-b15d9.firebaseio.com/products/${productId}.json?auth=${token}`,
       {
         method: 'DELETE'
       }
@@ -55,9 +56,9 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return async dispatch => {
-
-    const response = await fetch('https://rn-complete-guide-b15d9.firebaseio.com/products.json', {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const response = await fetch(`https://rn-complete-guide-b15d9.firebaseio.com/products.json?auth=${token}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -86,9 +87,10 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const response = await fetch(
-      `https://rn-complete-guide-b15d9.firebaseio.com/products/${id}.json`,
+      `https://rn-complete-guide-b15d9.firebaseio.com/products/${id}.json?auth=${token}`,
       {
         method: 'PATCH',
         headers: {
